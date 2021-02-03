5bb5c8e5a55fe92ecbfb4b4569d3b25c503d41a2
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2021-02-03 15:47:38 +0900

    :airplane: Nuxtのプロジェクトを追加

---
+++ nuxt.config.js
@@ -0,0 +1,37 @@
+export default {
+  target: "static",
+
+  srcDir: "src",
+
+  head: {
+    title: "yuarasino-homepage",
+    htmlAttrs: {
+      lang: "ja",
+    },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
+
+  css: [],
+
+  plugins: [],
+
+  components: true,
+
+  buildModules: ["@nuxt/typescript-build"],
+
+  modules: [],
+
+  build: {},
+
+  watchers: {
+    webpack: {
+      aggregateTimeout: 300,
+      poll: 1000,
+    },
+  },
+}
